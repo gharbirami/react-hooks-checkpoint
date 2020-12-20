45b4b936d8e957330f2b3550c398e1c80eccc2be
Rami <raami.ghaarbi@gmail.com>
2020-12-20 18:56:33 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+import { moviesData } from './components/MoviesData';
+import Search from './components/Search';
+import MoviesList from './components/MovieList';
+import AddMovie from './components/AddMovie';
+
+import './App.css';
+
+function App() {
+const [moviesList, setMoviesList] = useState(moviesData);
+const [searchMovie, setSearchMovie] = useState('');
+const [ratingSearch, setRatingSearch] = useState(1);
+
+const AddNewMovie = (e, newMovie) => {
+  e.preventDefault();
+  setMoviesList([...moviesList, newMovie]);
+};
+
+    return (
+      <div className='App'>
+        <h1 className="tunibest">TuniBest</h1>
+        <Search
+          setSearchMovie={setSearchMovie}
+          ratingSearch={ratingSearch}
+          setRatingSearch={setRatingSearch}
+        />
+        <AddMovie AddNewMovie={AddNewMovie} />
+        <MoviesList
+          moviesList={moviesList}
+          searchMovie={searchMovie}
+          ratingSearch={ratingSearch}
+        />
+      </div>
+    );
+}
+
+export default App;
